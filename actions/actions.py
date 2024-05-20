9d2ab8b737531fc0beaad87e93d9bd59543bd279
Viktor Scherbakov <viktoroo.sch@gmail.com>
2024-05-21 01:41:43 +0200

    lookup on data period action implemented

--- actions/actions.py
+++ actions/actions.py
@@ -1,15 +1,27 @@
-from rasa_sdk import Action
+import tempfile
+from typing import Any, Text, Dict, List
+
+import dateparser
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from rasa_sdk import Action, Tracker
 from rasa_sdk.events import UserUtteranceReverted
 from rasa_sdk.interfaces import Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 from rasa_sdk.types import DomainDict
+
+PAKISTAN_DATA = pd.read_csv('datasets/pakistan.csv')
+PAKISTAN_DATA.date = pd.to_datetime(PAKISTAN_DATA.date)
+PAKISTAN_DATA.price = PAKISTAN_DATA.price.astype(float)
 
 
 class ActionRepeatIntent(Action):
     def name(self):
         return "action_repeat_intent"
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict):
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict) -> List[Dict[Text, Any]]:
         # Fetch the latest intent
         intent_name = tracker.latest_message['intent'].get('name')
 
@@ -25,6 +37,67 @@ class ActionRepeatIntent(Action):
         # Send the message
         dispatcher.utter_message(text=message)
         return []
+
+
+class ActionAnalyzePrices(Action):
+
+    def name(self) -> Text:
+        return "action_analyze_prices"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict) -> List[Dict[Text, Any]]:
+        country = tracker.get_slot('country').lower()
+        commodity = tracker.get_slot('commodity').lower()
+        start_date = tracker.get_slot('start_date').lower()
+        end_date = tracker.get_slot('end_date').lower()
+
+        if country != 'pakistan':
+            dispatcher.utter_message(text=f'The country {country} is not supported yet. Sorry!')
+            return []
+
+        dates = [start_date, end_date]
+        dates_absolute = sorted(map(dateparser.parse, dates))
+
+        start_date, end_date = dates_absolute
+        filtered_df = PAKISTAN_DATA[(PAKISTAN_DATA['commodity'] == commodity) &
+                                    (PAKISTAN_DATA['date'] >= start_date) &
+                                    (PAKISTAN_DATA['date'] <= end_date)].reset_index()
+
+        if not len(filtered_df):
+            dispatcher.utter_message(text=f'No data found for the period from {start_date} to {end_date}')
+            return []
+
+        start_price = float(filtered_df.price[0])
+        end_price = float(filtered_df.price[len(filtered_df) - 1])
+
+        # Plotting the price dynamic using seaborn
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=filtered_df, x='date', y='price', marker='o')
+
+        # Customizing the plot
+        plt.title(f'Price Dynamics for {commodity.capitalize()}')
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+        plt.xticks(rotation=45)
+        plt.grid(True)
+
+        trend = 'up' if end_price > start_price else 'down'
+
+        # Save the plot to a temporary file
+        with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as f:
+            plt.savefig(f.name)
+            image_path = f.name
+
+        # Set slots with analysis result
+        return [
+            SlotSet('start_price', start_price),
+            SlotSet('end_price', end_price),
+            SlotSet('country', country),
+            SlotSet('commodity', commodity),
+            SlotSet('start_date', start_date.strftime("%Y-%m-%d")),
+            SlotSet('end_date', end_date.strftime("%Y-%m-%d")),
+            SlotSet('trend', trend),
+            SlotSet('image_path', image_path)
+        ]
 
 
 class ActionFallback(Action):
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import requests
 
 
 # Function to update conversation on the screen
-def update_conversation(user_message, bot_message):
+def update_conversation(user_message, bot_message, image_path=None):
     # Check if 'conversation' is already in the state
     if 'conversation' not in st.session_state:
         st.session_state.conversation = []
 
     # Append user and bot responses to the conversation history
-    st.session_state.conversation.append((user_message, bot_message))
+    st.session_state.conversation.append((user_message, bot_message, image_path))
 
 
 # Title of your web application
@@ -18,11 +18,12 @@ st.title('Rasa Chatbot')
 # Display message history
 if 'conversation' in st.session_state:
     conversation = []
-    for i, (message_user, message_bot) in enumerate(st.session_state.conversation):
+    for i, (message_user, message_bot, image_path) in enumerate(st.session_state.conversation):
         st.markdown('**' + message_user + '**')
         with st.container(border=True):
             st.markdown(message_bot)
-
+            if image_path:
+                st.image(image_path)
 
 # Text input for user message
 user_input = st.text_input("Type your message:")
@@ -42,8 +43,10 @@ if st.button("Send"):
 
         # Get bot response and update the conversation in session state
         if response.status_code == 200:
-            bot_response = response.json()[0]['text']  # Adjust based on response structure
-            update_conversation(user_input, bot_response)
+            response_json = response.json()
+            bot_response = response_json[0]['text']  # Adjust based on response structure
+            image_path = response_json[1].get('image')
+            update_conversation(user_input, bot_response, image_path)
             st.rerun()
         else:
             error_message = "Failed to get response from the bot."
